61d16ea8bffedc5854ffd2d17c0a7845fb95e571
irygiels <tojestpozadanylogin@gmail.com>
2017-11-06 14:10:42 +0100

    Initial commit

---
+++ app/src/main/java/telemisie/doorbell/DoorbellActivity.java
@@ -0,0 +1,79 @@
+package telemisie.doorbell;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.google.android.things.contrib.driver.button.Button;
+import com.google.android.things.contrib.driver.button.ButtonInputDriver;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
+public class DoorbellActivity extends Activity {
+
+    /*
+     * Driver for the doorbell button;
+     */
+    private ButtonInputDriver mButton;
+
+    /**
+     * The GPIO pin to activate for button presses.
+     */
+    private final String BUTTON_GPIO_PIN = ""; //tutaj wpisac numer pinu GPIO
+
+    /**
+     * A Handler for running tasks in the background.
+     */
+    private Handler mCameraHandler;
+    /**
+     * An additional thread for running tasks that shouldn't block the UI.
+     */
+    private HandlerThread mCameraThread;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "Doorbell Activity created.");
+
+        // Initialize the doorbell button driver
+        try {
+            mButton = new ButtonInputDriver(BUTTON_GPIO_PIN,
+                    Button.LogicState.PRESSED_WHEN_LOW, KeyEvent.KEYCODE_ENTER); // The keycode to send
+        } catch (IOException e) {
+            Log.e(TAG, "button driver error", e);
+        }
+
+        // Creates new handlers and associated threads for camera and networking operations.
+        mCameraThread = new HandlerThread("CameraBackground");
+        mCameraThread.start();
+        mCameraHandler = new Handler(mCameraThread.getLooper());
+    }
+
+    /**
+     * Override key event callbacks.
+     */
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Doorbell rang!
+            Log.d(TAG, "button pressed");
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCameraThread.quitSafely();
+        try {
+            mButton.close();
+        } catch (IOException e) {
+            Log.e(TAG, "button driver error", e);
+        }
+    }
+}
