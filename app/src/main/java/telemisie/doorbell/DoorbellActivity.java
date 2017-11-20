1883cd5143cad3ce7d8933f9c2c686463d635672
irygiels <tojestpozadanylogin@gmail.com>
2017-11-20 21:50:30 +0100

    button functionality

--- app/src/main/java/telemisie/doorbell/DoorbellActivity.java
+++ app/src/main/java/telemisie/doorbell/DoorbellActivity.java
@@ -1,41 +1,28 @@
 package telemisie.doorbell;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.widget.Toast;
 
 import com.google.android.things.contrib.driver.button.Button;
 import com.google.android.things.contrib.driver.button.ButtonInputDriver;
 
 import java.io.IOException;
 
-import static android.content.ContentValues.TAG;
-
 public class DoorbellActivity extends Activity {
 
+    private static final String TAG = "BUTTON";
     /*
-     * Driver for the doorbell button;
-     */
+         * Driver for the doorbell button;
+         */
     private ButtonInputDriver mButton;
 
     /**
      * The GPIO pin to activate for button presses.
      */
-    private final String BUTTON_GPIO_PIN = ""; //tutaj wpisac numer pinu GPIO
+    private final String BUTTON_GPIO_PIN = "GPIO_39";
 
-    /**
-     * A Handler for running tasks in the background.
-     */
-    private Handler mCameraHandler;
-    /**
-     * An additional thread for running tasks that shouldn't block the UI.
-     */
-    private HandlerThread mCameraThread;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,11 +35,6 @@ public class DoorbellActivity extends Activity {
         } catch (IOException e) {
             Log.e(TAG, "button driver error", e);
         }
-
-        // Creates new handlers and associated threads for camera and networking operations.
-        mCameraThread = new HandlerThread("CameraBackground");
-        mCameraThread.start();
-        mCameraHandler = new Handler(mCameraThread.getLooper());
     }
 
     /**
@@ -63,12 +45,6 @@ public class DoorbellActivity extends Activity {
         if (keyCode == KeyEvent.KEYCODE_ENTER) {
             // Doorbell rang!
             Log.d(TAG, "button pressed");
-            Context cont = getApplicationContext();
-            CharSequence text = "Button is pressed!";
-            int duration = Toast.LENGTH_SHORT;
-
-            Toast toast = Toast.makeText(cont, text, duration);
-            toast.show();
             return true;
         }
         return super.onKeyUp(keyCode, event);
@@ -77,7 +53,7 @@ public class DoorbellActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mCameraThread.quitSafely();
+
         try {
             mButton.close();
         } catch (IOException e) {
